3e2b13bfe020580af3b421098c39cb25299a178a
suman123 <sumanrajravi@bitcot.com>
2024-02-04 21:16:30 +0530

    dummy test

--- cypress/e2e/account_service.cy.js
+++ cypress/e2e/account_service.cy.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-import { getUserByIdQuery } from '../support/address';
-import { graphql } from '../support/graphql_util';
+import { getUserByIdQuery } from "../support/address";
+import { graphql } from "../support/graphql_util";
 
-describe('GraphQL Test', () => {
-  it('should retrieve user by ID', () => {
+describe("GraphQL Test", () => {
+  it("should retrieve user by ID", () => {
     // const userId = 'U-9496115-DEV';
     // const query = graphql(getUserByIdQuery(userId));
 
-    cy.visit('https://google.com')
+    cy.visit("https://www.amazon.in/");
   });
 });
